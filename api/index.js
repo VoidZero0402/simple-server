1af8401467c9137ba2b79b97c641460a48bcb6c7
VoidZero0402 <voidzero0204@gmail.com>
2025-05-23 18:38:54 +0330

    chore: two api for vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    res.json({ message: "Hello From Vercel!" });
+});
+
+app.get("/products", (req, res) => {
+    res.json({
+        data: [
+            { id: 1, name: "product-1" },
+            { id: 2, name: "product-2" },
+        ],
+    });
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
